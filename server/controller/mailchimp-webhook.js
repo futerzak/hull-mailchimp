ae561280fe71be02554d94d1ad4e59e6129e5a4c
Stephane Bellity <sbellity@gmail.com>
2016-10-14 11:33:09 +0200

    sunscribed / unsubscribed status

--- server/controller/mailchimp-webhook.js
+++ server/controller/mailchimp-webhook.js
@@ -4,6 +4,7 @@ import flatten from "flat";
 export default class MailchimpWebhookController {
 
   handleAction(req, res) {
+    const { type, data } = req.body || {};
     const listId = _.get(req, "hull.ship.private_settings.mailchimp_list_id");
 
     if (!listId) {
@@ -11,14 +12,16 @@ export default class MailchimpWebhookController {
       return res.end("Not found");
     }
 
-    const { type, data } = req.body || {};
+    if (!data || !data.email) {
+      res.status(404);
+      return res.end("Email not found");
+    }
 
-    if (type === "profile" || type === "subscribe" && data && data.email) {
-      const { email, id } = data;
-      const anonymous_id = `mailchimp:${id}`;
+    const { email, id } = data;
+    const anonymous_id = `mailchimp:${id}`;
+    const hull = req.hull.client.as({ email, anonymous_id });
 
-      const hull = req.hull.client.as({ email, anonymous_id });
-
+    if (type === "profile" || type === "subscribe") {
       const merges = _.omit(data.merges, "GROUPINGS");
       if (merges.INTERESTS) {
         merges.INTERESTS = merges.INTERESTS.split(",").map(_.trim);
@@ -27,12 +30,19 @@ export default class MailchimpWebhookController {
       const traits = _.reduce({
         updated_at: new Date().toISOString(),
         unique_email_id: id,
+        subscribed: true,
+        status: "subscribed",
         ...flatten(merges, { delimiter: "_", safe: true })
       }, (tt, v, k) => {
         return { ...tt, [`mailchimp/${k.toLowerCase()}`]: v };
       }, {});
 
       hull.traits(traits);
+    } else if (type === "unsubscribe") {
+      hull.traits({
+        status: "unsubscribed",
+        subscribed: false
+      }, { source: "mailchimp" });
     }
 
     return res.json({ listId, ok: true });
