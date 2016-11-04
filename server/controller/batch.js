f92a5c5a1d10fb53db4e4c59075c091a300a812c
Stephane Bellity <stephane@hull.io>
2016-11-04 08:37:49 +0100

    error logging

--- server/controller/batch.js
+++ server/controller/batch.js
@@ -81,8 +81,8 @@ export default class BatchController {
       .then(() => {
         return mailchimpAgent.saveToAudiences(usersToAddOrRemove);
       })
-      .catch((err) => {
-        console.log("sendUsersJob.error", err);
+      .catch((err = {}) => {
+        console.log("sendUsersJob.error", err.message);
         return Promise.reject(err);
       });;
   }
